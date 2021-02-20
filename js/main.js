5dfb7a709d7b9f4f10e3196fd3dc56ea608dd6d1
jairoMolina9 <molinaandres9991@gmail.com>
2021-02-19 19:50:56 -0500

    testing jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(document).ready(() => {
     console.log('ready');
-    $('.navbar_wrapper').load('https://jairomolina9.github.io/thp-site/components/navbar.html');
+    $('.navbar_wrapper').load('/thp-site/components/navbar.html');
 });
